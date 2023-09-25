f0cb12fd3cfffd70197892419ec01e394115b896
Raphael <raphael.campagnac@live.fr>
2023-09-25 14:57:26 +0200

    refacto stars

--- src/components/stars/stars.tsx
+++ src/components/stars/stars.tsx
@@ -3,121 +3,19 @@ import styles from './stars.module.css'
 
 export default function Stars() {
 
-    const numberOfStar = 50;
+    function GetStarGroup(numberOfStar : number) {
+        return (
+            [...Array(numberOfStar)].map((value: undefined, index: number) => (
+                <Star key={index} />
+            ))            
+    );
+    }
 
     return(
     <main className={styles.universe}>
-        <div className={styles.tel}>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-        <div className={styles.tablette}>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-        <div className={styles.pc}>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
+        <div className={styles.tel}>{GetStarGroup(30)}</div>
+        <div className={styles.tablette}>{GetStarGroup(50)}</div>
+        <div className={styles.pc}>{GetStarGroup(50)}</div>
     </main>  
     )
 }
