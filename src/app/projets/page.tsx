2ebd0c51eb05de3a22caa93938b3c69c5a9e4187
Raphael <raphael.campagnac@live.fr>
2023-10-04 12:24:52 +0200

    remove double navbar

--- src/app/projets/page.tsx
+++ src/app/projets/page.tsx
@@ -3,7 +3,6 @@ import Navbar from '@/components/navbar/navbar'
 export default function PageProjets() {
   return (
     <main>
-      <Navbar/>
   </main>
   )
 }
--- src/app/sites/page.tsx
+++ src/app/sites/page.tsx
@@ -3,7 +3,6 @@ import Navbar from '@/components/navbar/navbar'
 export default function PageSites() {
   return (
     <main>
-      <Navbar/>
   </main>
   )
 }
