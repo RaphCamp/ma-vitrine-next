d333cc77a9101a53fe7366cf000eb8445c551d9b
Raphael <raphael.campagnac@live.fr>
2023-10-04 12:23:34 +0200

    change favico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import Image from 'next/image'
-import musicOn from '../assets/images/musicOn.png'
-import codewars from '../assets/images/codewars.svg'
 import Intro from '@/components/intro/intro'
-import Navbar from '@/components/navbar/navbar'
-import Panels from '@/components/panels/panels'
-import Doors from '@/components/doors/Doors'
 
 export default function PageHome() {
   return (
